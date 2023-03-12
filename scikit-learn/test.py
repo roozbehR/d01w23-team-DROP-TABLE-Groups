19ca1d015bd636824605ee6246a2c46f49754967
roozbehR <61944934+roozbehR@users.noreply.github.com>
2023-03-12 16:53:33 -0400

    Deleting test.py as it was a dummy file we added and is not needed

--- scikit-learn/test.py
+++
@@ -1,6 +0,0 @@
-from sklearn import clone
-from sklearn.feature_extraction.text import CountVectorizer
-
-cntvec = CountVectorizer(vocabulary={'g': 0, 'a': 1, 't': 2, 'c': 3})
-print(cntvec.vocabulary is clone(cntvec, safe=False).vocabulary)
-
