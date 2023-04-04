b766a80d280dc6ccdb271a264166a9b6b3b945c0
roozbehR <61944934+roozbehR@users.noreply.github.com>
2023-04-04 01:20:49 -0400

    Adding more acceptance tests

--- scikit-learn/sklearn/feature_selection/tests/test_rfe.py
+++ scikit-learn/sklearn/feature_selection/tests/test_rfe.py
@@ -30,7 +30,7 @@ from sklearn.metrics import make_scorer
 from sklearn.metrics import get_scorer
 
 
-class MockClassifier:
+class MockClassifier(BaseEstimator):
     """
     Dummy classifier to test recursive feature elimination
     """
